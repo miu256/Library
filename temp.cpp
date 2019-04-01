fa394e1a7c78d9d5ec798f74776844120f9da882
Rita Gunji <4317056@ed.tus.ac.jp>
2019-04-01 16:48:10 +0900

    Template for library code

---
+++ temp.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+/**
+ *
+ *
+ *
+ *
+ *
+ * @param
+ * @return
+ **/
+
+
+// CUT begin
+
+//CUT end
+
+
+int main() {
+
+
+    return 0;
+}
